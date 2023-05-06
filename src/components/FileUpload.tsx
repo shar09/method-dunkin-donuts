e167bc8efde70f0de5c903da663f50206d541268
Sharath Telu <sharathtelu9@gmail.com>
2023-05-05 21:31:47 -0400

    create api method calls

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import convert from 'xml-js';
+import api from '../lib/api';
 
 export function FileUpload () {
     const readUrl = async () => {
-        const url = 'https://file.notion.so/f/s/8c08a999-4b9e-44b8-bc17-bbaf8c219101/dunkin.xml?id=377557b0-66f2-45d7-a159-b2381391bfa2&table=block&spaceId=d0d5787b-ff93-48d4-bb8d-bffd9edc42e4&expirationTimestamp=1683320159541&signature=M6sQWD_enEmfbdjt3j_SRsFoZsHSNvNW7VjeA4z9daI&downloadName=dunkin.xml';
+        const url = 'https://file.notion.so/f/s/8c08a999-4b9e-44b8-bc17-bbaf8c219101/dunkin.xml?id=377557b0-66f2-45d7-a159-b2381391bfa2&table=block&spaceId=d0d5787b-ff93-48d4-bb8d-bffd9edc42e4&expirationTimestamp=1683421747391&signature=yLW2hHpLywlbocDo20o9YxDgRxsB918PYO0mUZKOWME&downloadName=dunkin.xml';
         const res = await fetch(url);
         const xml = await res.text();
         const result1 = convert.xml2json(xml, {compact: true, spaces: 4});
@@ -19,22 +20,17 @@ export function FileUpload () {
             const groupBySource = () => {
                 const groupedResults = Object.values(
                     data.root.row.reduce((acc: any, item: any) => {
-                        acc[item.Payor.AccountNumber._text] = [...(acc[item.Payor.AccountNumber._text] || []), item];
+                        acc[item.Employee.DunkinId._text] = [...(acc[item.Employee.DunkinId._text] || []), item];
                         return acc;
                     },{})
                 );
                 return groupedResults;
             }
             const grouped: any[] = groupBySource();
+            // console.log(grouped);
 
-            let res: any[] = [];
-            for(let element of grouped) {
-                res = res.concat(element.filter((item: any) => {
-                    return item.Employee.DunkinId._text === 'EMP-c8d91f03-594f-43cf-972a-7db7fac8124a'
-                }));
-            }
-
-            console.log(res);
+            const entities = await api.getEntities({});
+            console.log(entities);
 
           })();
     }, [])
---
+++ src/lib/api.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+const url = "https://dev.methodfi.com";
+
+const api_key = process.env.REACT_APP_API_KEY
+  ? process.env.REACT_APP_API_KEY
+  : "";
+
+const config = {
+  Authorization: `Bearer ${api_key}`,
+  "Content-type": "application/json",
+};
+
+const api = {
+  /**
+   * Get list of entities
+   * @param options type, status
+   * @returns list of existing entities
+   */
+  getEntities: async (options: { type?: string; status?: string }) => {
+    const entities = await axios.get(`${url}/entities`, {
+      params: options,
+      headers: config,
+    });
+    return entities;
+  },
+
+  /**
+   * Create an entity
+   * @param options entity details object
+   * @returns creates a new entity
+   */
+  createEntity: async (options: any) => {
+    return await axios.post(`${url}/entities`, options, {
+      headers: config,
+    });
+  },
+
+  /**
+   * Create an account
+   * @param options account details object
+   * @returns creates a bank account for payment transfers
+   */
+  createAccount: async (options: any) => {
+    return await axios.post(`${url}/accounts`, options, {
+      headers: config,
+    });
+  },
+
+  /**
+   * Make Payment
+   * @param options payment details object
+   * @returns makes payment for given account id's
+   */
+  makePayment: async (options: any) => {
+    return await axios.post(`${url}/payments`, options, {
+      headers: config,
+    });
+  },
+};
+
+export default api;
